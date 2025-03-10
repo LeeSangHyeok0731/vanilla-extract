ec95c061a3c2ec8deee6dd0e0662d5b14990720b
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-03-10 22:49:21 +0900

    적용

---
+++ src/app/login/login.tsx
@@ -0,0 +1,9 @@
+import * as S from "./login.css";
+
+export default function Login() {
+  return (
+    <div className={S.Container}>
+      <div className={S.RedItems} /> <div className={S.GreyItems} />
+    </div>
+  );
+}
